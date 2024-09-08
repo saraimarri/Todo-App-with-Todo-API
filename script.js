96432c95e76cb1d6d38140201b98ba7dd54b36eb
sara imarri <sara.3gi@gmail.com>
2024-09-08 16:58:37 +0200

    todo app with api

--- script.js
+++ script.js
@@ -1,15 +1,50 @@
-const newTodoInput= document.querySelector("new-todo");
-const addTodoBtn = document.querySelector("new-todo-btn");
+const newTodoInput = document.querySelector("#new-todo");
+const addtodoBtn = document.querySelector("#new-todo-btn");
+const todoList = document.querySelector("#list");
 
-
+let todos = [];
 
 function loadTodos() {
-fetch("http://localhost:3000/todos")
-.then((res)=> res.json())
-.then(todos => {
-console.log(todos);
-});
+  fetch("http://localhost:3000/todos")
+    .then((res) => res.json())
+    .then((todosFromApi) => {
+     // console.log(todosFromApi)
+      todos = todosFromApi;
+      renderTodos();
+    });
 }
 
+function renderTodos() {
+  todoList.innerHTML = "";
+  todos.forEach((todo) => {
+    const newLi = document.createElement("li");
+    const text = document.createTextNode(todo.description);
+    newLi.appendChild(text);
+    todoList.appendChild(newLi);
+  });
+}
+
+addtodoBtn.addEventListener("click", () => {
+  const newTodoText = newTodoInput.value;
+  const newTodo = {
+    description: newTodoText,
+    done: false,
+  };
+
+  fetch("http://localhost:3000/todos", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newTodo),
+  })
+    .then((res) => res.json())
+    .then((newTodoFromApi) => {
+      todos.push(newTodoFromApi);
+      renderTodos();
+    });
+});
 
 loadTodos();
+
+
